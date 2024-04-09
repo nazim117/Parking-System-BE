1d57e0ddab9e2d0c964075b38ba2f40ce95408f5
Danila Solovenko <dansil2301@gmail.com>
2024-04-09 21:19:47 +0200

    small refactoring in email messages

--- src/main/java/S3/eco/parking_system/utils/EmailMessages.java
+++ src/main/java/S3/eco/parking_system/utils/EmailMessages.java
@@ -2,13 +2,14 @@ package S3.eco.parking_system.utils;
 
 public class EmailMessages {
     public static final String APPOINTMENT_CONFIRMATION_SUBJECT = "Appointment confirmation";
+
     public static final String APPOINTMENT_CONFIRMATION_BODY = "Hello (name of the guest), your appointment is scheduled for (date) at (time).";
 
     public static final String APPOINTMENT_REMINDER_SUBJECT = "Appointment reminder";
+
     public static final String APPOINTMENT_REMINDER_BODY = "Hello (guest name), just a friendly reminder about your appointment on (date) at (time)";
 
     public static final String APPOINTMENT_ASSISTANCE_SUBJECT = "Assistance notification";
+
     public static final String APPOINTMENT_ASSISTANCE_BODY = "(Here guest will get notif if we have enough space for his transport)";
-
-
 }
